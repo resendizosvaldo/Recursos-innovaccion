64cb72c15150b414a449a777649c91e2ae465274
ISC. Osvaldo Resendiz <cev6732@innovaccion.mx>
2021-10-16 20:17:33 -0500

    Update segundapractica.py

--- Python/segundapractica.py
+++ Python/segundapractica.py
@@ -1,17 +1,36 @@
-#IMPRIMIR TU NOMBRE
-nombre = input("Introduce tu nombre: ")
-#CONCATENACION - PONER UNA VARIABLE JUNTO CON UN TEXTO PLANO
-print(f"Hola {nombre}")
-print("Hola {}".format(nombre))
-print("Hola " + nombre)
 
-#ENTERO
-edad = input("Ahora introduce tu edad: ")
-#FLOTANTE DECIMALES
-estatura = 1.75
-#CONVERTIR FLOTANTE
-edadString = str(edad)#Palabra que contiene el 2 y el 5 (25)
-booleanos = False #True
+def validacionEdad() :
+    #IMPRIMIR TU NOMBRE
+    nombre = input("Introduce tu nombre: ")
+    #CONCATENACION - PONER UNA VARIABLE JUNTO CON UN TEXTO PLANO
+    print(f"Hola {nombre}")
+    print("Hola {}".format(nombre))
+    print("Hola " + nombre)
 
-print(type(edad))
-print(type(edadString))
+    #ENTERO
+    edad = input("Ahora introduce tu edad: ")
+    #FLOTANTE DECIMALES
+    estatura = 1.75
+    #CONVERTIR FLOTANTE
+    edadString = str(edad)#Palabra que contiene el 2 y el 5 (25)
+    booleanos = False #True
+
+    print(type(edad))
+    print(type(edadString))
+
+    #transformar string a entero
+    edad = int(edad)
+
+    #ESTRUCTURA DE CONTROL IF
+    if edad >= 18 and edad < 100: #Si tu edad es mayor o igual a 18 o menor a 100
+        print("Hola {} pasale por tu tonayan".format(nombre)) #Hará esto
+    elif edad >= 100 : #Si no, entonces ¿Edad es mayor  o igual a 100?
+        print("¿Acaso eres chabelo?")#Hará esto
+    elif edad <= 0 :
+        print("Aún ni naces mi chavo, te la bañaste")
+    else :
+        print("Estás chavo mi buen")
+    #ESTRUCTURA DE CONTROL FOR
+
+while True :
+    validacionEdad()
