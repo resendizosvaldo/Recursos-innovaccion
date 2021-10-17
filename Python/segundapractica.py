7ac661a649ed7b0b0b9f3b70e67bb999c0f4c5ab
ISC. Osvaldo Resendiz <cev6732@innovaccion.mx>
2021-10-16 20:07:16 -0500

    Create segundapractica.py

---
+++ Python/segundapractica.py
@@ -0,0 +1,17 @@
+#IMPRIMIR TU NOMBRE
+nombre = input("Introduce tu nombre: ")
+#CONCATENACION - PONER UNA VARIABLE JUNTO CON UN TEXTO PLANO
+print(f"Hola {nombre}")
+print("Hola {}".format(nombre))
+print("Hola " + nombre)
+
+#ENTERO
+edad = input("Ahora introduce tu edad: ")
+#FLOTANTE DECIMALES
+estatura = 1.75
+#CONVERTIR FLOTANTE
+edadString = str(edad)#Palabra que contiene el 2 y el 5 (25)
+booleanos = False #True
+
+print(type(edad))
+print(type(edadString))
